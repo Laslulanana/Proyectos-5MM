130a396ffd7e236f642a3ae008151cc6f10d43e3
Manuel Santiso <manuelsantiso0@gmail.com>
2021-07-05 20:38:27 +0200

    subiendo

---
+++ Survivor Z/Assets/ControladorImpulsoVelocidad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorImpulsoVelocidad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fuerzaImpulso = 3;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>() != null)
+        {
+            other.GetComponent<Rigidbody2D>().AddForce(transform.up * fuerzaImpulso, ForceMode2D.Impulse);
+        }
+
+    }
+
+}
+
